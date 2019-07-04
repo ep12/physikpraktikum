bcb6edc360e1b99a7d16de9c03fa4f332bcfae6b
ep12 <epsilonproducts12@gmail.com>
2019-07-04 19:04:55 +0200

    siunits: use abstract unit system

--- measured/units/siunits.py
+++ measured/units/siunits.py
@@ -1,36 +1,41 @@
-from physikpraktikum.measured.units.unit import Unit, UnitComposition, named_unit_composition as compose, find_single_combined_unit
+from physikpraktikum.measured.units.unit import Unit, UnitComposition, UnitSystem
+#from physikpraktikum.measured.units.unit import Unit, UnitComposition, named_unit_composition as compose, find_single_combined_unit
 
-Meter = Unit('Length', 'Meter', 'm', r'\meter')
-Kilogram = Unit('Mass', 'Kilogram', 'kg', r'\kilo\gram')
-Second = Unit('Time', 'Second', 's', r'\second')
-Ampere = Unit('Electric current', 'Ampere', 'A', r'\ampere')
-Kelvin = Unit('Thermodynamic temperature', 'Kelvin', 'K', r'\kelvin')
-K = Kelvin
-Mole = Unit('Amount of substance', 'Mole', 'mol', r'\mole')
-Candela = Unit('Luminous intensity', 'Candela', 'cd', 'r\candela')
+SI = UnitSystem('SI International System of Units')
+
+Meter = SI('Length', 'Meter', 'm', r'\meter')
+Kilogram = SI('Mass', 'Kilogram', 'kg', r'\kilo\gram')
+Second = SI('Time', 'Second', 's', r'\second')
+Ampere = SI('Electric current', 'Ampere', 'A', r'\ampere')
+Kelvin = SI('Thermodynamic temperature', 'Kelvin', 'K', r'\kelvin')
+Mole = SI('Amount of substance', 'Mole', 'mol', r'\mole')
+Candela = SI('Luminous intensity', 'Candela', 'cd', 'r\candela')
 
 ############################################################################################
 
-Hertz = compose(1 / Second, 'Frequency', 'Hertz', 'Hz', r'\hertz')
-Becquerel = compose(1 / Second, 'Radioactivity', 'decays per second', 'Bq', r'\hertz') # TODO: siunitx support?
-Coulomb = compose(Ampere * Second, 'Electric charge', 'Coulomb', 'C', r'\coulomb')
-Newton = compose(Kilogram * Meter / Second ** 2, 'Force', 'Newton', 'N', r'\newton')
+Radian = SI('Planar angle', 'Radian', 'rad', r'\radian', Meter / Meter) # TODO: siunitx support?
+Steradian = SI('Solid angle', 'Steradian', 'sr', r'\steradian', Meter ** 2 / Meter ** 2) # TODO: siunitx support?
 
-Pascal = compose(Newton / Meter ** 2, 'Pressure', 'Pascal', 'Pa', r'\pascal') # TODO: siunitx support?
-Joule = compose(Newton * Meter, 'Energy', 'Joule', 'J', r'\joule') # TODO: siunitx?
-Watt = compose(Joule / Second, 'Power', 'Watt', 'W', r'\watt') # TODO: siunitx?
-Volt = compose(Joule / Coulomb, 'Voltage', 'Volt', 'V', r'\volt')
-Farad = compose(Coulomb / Volt, 'Electric capacitance', 'Farad', 'F', r'\farad')
-Ohm = compose(Volt / Ampere, 'Electric resistance', 'Ohm', 'Ω', r'\ohm')
-Siemens = compose(1 / Ohm, 'Eletric conductance', 'Siemens', 'S', r'\siemens') # TODO: siunitx?
-Weber = compose(Volt * Second, 'Magnetic flux', 'Weber', 'Wb', r'\weber') # TODO: siunitx?
-Tesla = compose(Weber / Meter ** 2, 'Magnetic induction', 'Tesla', 'T', r'\tesla') # TODO: siunitx?
-Henry = compose(Weber / Ampere, 'Electric inductance', 'Henry', 'H', r'\henry') # TODO: siunitx?
-Gray = compose(Joule / Kilogram, 'Absorbed dose of ionizing radiation', 'Gray', 'Gy', r'\gray') # TODO: siunitx?!
-Sievert = compose(Joule / Kilogram, 'Health effect of ionizing radiation', 'Sievert', 'Sv', r'\sievert') # TODO: siunitx?
-Katal = compose(Mole / Second, 'Catalytic activity', 'Katal', 'cat', r'\katal') # TODO: siunitx?
+Hertz = SI('Frequency', 'Hertz', 'Hz', r'\hertz', 1 / Second)
+Becquerel = SI('Radioactivity', 'Becquerel', 'Bq', r'\hertz', 1 / Second) # TODO: siunitx support?
+Coulomb = SI('Electric charge', 'Coulomb', 'C', r'\coulomb', Ampere * Second)
+Newton = SI('Force', 'Newton', 'N', r'\newton', Kilogram * Meter / Second ** 2)
 
-all_well_known_units = [v for k, v in locals().copy().items() if '__' not in k and isinstance(v, (Unit, UnitComposition))]
+Pascal = SI('Pressure', 'Pascal', 'Pa', r'\pascal', Newton / Meter ** 2) # TODO: siunitx support?
+Joule = SI('Energy', 'Joule', 'J', r'\joule', Newton * Meter) # TODO: siunitx?
+Watt = SI('Power', 'Watt', 'W', r'\watt', Joule / Second) # TODO: siunitx?
+Volt = SI('Voltage', 'Volt', 'V', r'\volt', Joule / Coulomb)
+Farad = SI('Electric capacitance', 'Farad', 'F', r'\farad', Coulomb / Volt)
+Ohm = SI('Electric resistance', 'Ohm', 'Ω', r'\ohm', Volt / Ampere)
+Siemens = SI('Eletric conductance', 'Siemens', 'S', r'\siemens', 1 / Ohm) # TODO: siunitx?
+Weber = SI('Magnetic flux', 'Weber', 'Wb', r'\weber', Volt * Second) # TODO: siunitx?
+Tesla = SI('Magnetic induction', 'Tesla', 'T', r'\tesla', Weber / Meter ** 2) # TODO: siunitx?
+Henry = SI('Electric inductance', 'Henry', 'H', r'\henry', Weber / Ampere) # TODO: siunitx?
+Gray = SI('Absorbed dose of ionizing radiation', 'Gray', 'Gy', r'\gray', Joule / Kilogram) # TODO: siunitx?!
+Sievert = SI('Health effect of ionizing radiation', 'Sievert', 'Sv', r'\sievert', Joule / Kilogram) # TODO: siunitx?
+Katal = SI('Catalytic activity', 'Katal', 'cat', r'\katal', Mole / Second) # TODO: siunitx?
+
+#all_well_known_units = [v for k, v in locals().copy().items() if '__' not in k and isinstance(v, (Unit, UnitComposition))]
 
 
 #for u in all_well_known_units:
@@ -82,9 +87,11 @@ if __name__ == '__main__':
     #    print(x, repr(x))
     #print(Joule / (Kilogram * Kelvin))
     #print(Weber / Meter ** 2 == Tesla)
-    print('HERE' *  10)
+    print('HERE ' *  10)
     #test = Joule / Weber
     test = Hertz / Newton
-    print(test._kwargs)
+    print(test)
+    print(SI)
     #for k, v in test._kwargs.items():
     #    print(k, v)
+    print(SI.str_vector_representation)
